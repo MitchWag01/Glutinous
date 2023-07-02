bf205f1f721bc1ad156a511a80451113b21052a0
jesst <jessterman666@gmail.com>
2023-07-02 10:56:24 -0600

    The searchbar stores into an array 'searchlist'

--- glutinous/glutinous/src/pages/Home.tsx
+++ glutinous/glutinous/src/pages/Home.tsx
@@ -5,20 +5,21 @@ import { useState } from 'react';
 import { Stack, Box } from '@mui/material';
 import MenuButton from '../components/Buttons/MenuButton';
 import ScanPage from './Scan';
-import SearchPage from './Searchpage'
+import SearchPage from './SearchPage';
 
 const Home = () => {
   const [ScanState, setScanState] = useState(false);
-  const [SearchState, setSearchState] = useState(false)
+  const [SearchState, setSearchState] = useState(false);
+  const [searchList, setSearchList] = useState<string[]>([]); // Define an array to hold the search items
 
   return (
     <Stack>
       <Header></Header>
-      <Box sx={{ position: 'absolute', top: 0, right: 0, }}>
-        <MenuButton setScanState={setScanState} setSearchState={setSearchState}/>
+      <Box sx={{ position: 'absolute', top: 0, right: 0 }}>
+        <MenuButton setScanState={setScanState} setSearchState={setSearchState} />
       </Box>
-      {ScanState && <ScanPage ingredientsList='Jesse Likes a reasonable amount of bread, too bad some people just cant handle it'/>}
-      {SearchState &&< SearchPage searchlist='="There is currently nothing right now'/>}
+      {ScanState && <ScanPage ingredientsList="Jesse Likes a reasonable amount of bread, too bad some people just cant handle it" />}
+      {SearchState && <SearchPage searchlist={searchList} />} 
     </Stack>
   );
 };
--- glutinous/glutinous/src/pages/SearchPage.tsx
+++ glutinous/glutinous/src/pages/SearchPage.tsx
@@ -1,18 +1,31 @@
 import { AccountCircle, AirlineSeatReclineNormalTwoTone } from '@mui/icons-material'
 import { Box, Stack, Typography } from '@mui/material'
-import React from 'react'
+import React, {useState} from 'react'
 import {TextField} from '@mui/material'
 import { SearchRounded } from '@mui/icons-material'
 
 interface SearchProp{
-    searchlist: string
+    searchlist: string[]
 }
 
 const SearchPage: React.FC<SearchProp> = ({searchlist})=>{
+    const [searchText, setSearchText] = useState('');
+    console.log(searchlist);
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' && searchText.trim() !== '') {
+      setSearchText('');
+      // Add searchText to the searchlist array
+      searchlist.push(searchText);
+    }
+  };
     return ( 
         <Box sx={{ display: 'flex', alignItems: 'flex-end', background:'#679ce3', borderRadius:'10px', height:'50px'}}>
         <SearchRounded sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-        <TextField id="input-with-sx" label="Search here" variant="standard" sx={{color: 'black'}} />
+        <TextField 
+        onChange={(e) => setSearchText(e.target.value)}
+        onKeyDown={handleKeyDown}
+        id="input-with-sx" label="Search here" variant="standard" sx={{color: 'black'}} />
       </Box>
     )
 }
