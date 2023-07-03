631951d6cda9b1bbccd7846243c9ba99e530968c
Mitchell Wagner <pim128@usask.ca>
2023-07-03 14:10:49 -0600

    Fixed screen positioning

--- glutinous/glutinous/src/App.tsx
+++ glutinous/glutinous/src/App.tsx
@@ -2,86 +2,18 @@ import React, { useState } from 'react';
 import { Box, Button, Stack, ThemeProvider } from '@mui/material';
 import Home from './pages/Home';
 import SearchButton from './components/Buttons/SearchButton';
-const API_KEY = "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"; // Replace with your actual API key
 import theme from './themes/theme';
 
 
 
 
 const App = () => {
-  const [ingredients, setIngredients] = useState("");
-  const [isGlutenFree, setIsGlutenFree] = useState("");
-
-  async function callOpenAIAPI() {
-    console.log("Calling API");
-
-    const prompt = `Is ${ingredients} gluten-free, Yes or No?`;
-
-    const APIBody = {
-      model: "text-davinci-003",
-      prompt,
-      temperature: 0,
-      max_tokens: 100,
-      top_p: 1,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-      stop: [" "]
-    };
-
-    try {
-      const response = await fetch("https://api.openai.com/v1/completions", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${API_KEY}`
-        },
-        body: JSON.stringify(APIBody)
-      });
-
-      const data = await response.json();
-      console.log(data);
-
-      if (data.choices && data.choices.length > 0) {
-        const generatedText = data.choices[0].text.trim();
-        setIsGlutenFree(generatedText);
-      } else {
-        setIsGlutenFree("No response from the API");
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      setIsGlutenFree("API call failed");
-    }
-  }
 
   return (
     <ThemeProvider theme={theme}>
-    <Stack>
-    <Box sx={{ 
-        display: "flex", 
-        justifyContent: "center", 
-        alignContent: "center", 
-        flexDirection: { xs: "column", md: "row" },
-        padding: { xs: "60px 10px 10px 10px", md: "20px 0 0 0" } 
-      }}>
-    <Home
-    />
-      <textarea
-        onChange={(e) => setIngredients(e.target.value)}
-        placeholder='Enter Your Ingredients List'
-        cols={75}
-        rows={10}
-      />
-      <Button onClick={callOpenAIAPI}>
-        SCAN
-      </Button>
-      
-      {isGlutenFree !== "" && (
-        <h3>
-          Are The ingredients Gluten-free? {isGlutenFree}
-        </h3>
-)}
-    </Box>
-    </Stack>
+      <Stack>
+        <Home/>
+      </Stack>
     </ThemeProvider>
   )
 };
--- glutinous/glutinous/src/pages/Home.tsx
+++ glutinous/glutinous/src/pages/Home.tsx
@@ -10,26 +10,93 @@ import NavBar from '../components/NavBar/Navbar';
 import { alignProperty } from '@mui/material/styles/cssUtils';
 import { AlignHorizontalCenter } from '@mui/icons-material';
 import MainCameraButton from '../components/Buttons/MainCameraButton';
+const API_KEY = "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"; // this needs to become an environment variable
+
+
 
 const Home = () => {
   const [ScanState, setScanState] = useState(false);
   const [SearchState, setSearchState] = useState(false);
   const [searchList, setSearchList] = useState<string[]>([]);
 
+
+  // API to chat GPT
+  const [ingredients, setIngredients] = useState("");
+  const [isGlutenFree, setIsGlutenFree] = useState("");
+
+  async function callOpenAIAPI() {
+    console.log("Calling API");
+
+    const prompt = `Is ${ingredients} gluten-free, Yes or No?`;
+
+    const APIBody = {
+      model: "text-davinci-003",
+      prompt,
+      temperature: 0,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: [" "]
+    };
+
+    try {
+      const response = await fetch("https://api.openai.com/v1/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify(APIBody)
+      });
+
+      const data = await response.json();
+      console.log(data);
+
+      if (data.choices && data.choices.length > 0) {
+        const generatedText = data.choices[0].text.trim();
+        setIsGlutenFree(generatedText);
+      } else {
+        setIsGlutenFree("No response from the API");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setIsGlutenFree("API call failed");
+    }
+    // onClick={callOpenAIAPI}
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Stack>
   
-        {ScanState && <ScanPage ingredientsList="Jesse Likes a reasonable amount of bread, too bad some people just cant handle it" />}
+        {ScanState && <ScanPage ingredientsList="Jesse's bread preferences are a terror to our society as he likes raisin bread..." />}
         {SearchState && <SearchPage searchlist={searchList} />} 
         
-        <Box sx={{ position: 'fixed',bottom: 0, left: '41%', right:'60%' }}>
-          <MainCameraButton/>
+        <Box       sx={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)'
+      }}>
+          <MainCameraButton />
+          {isGlutenFree !== "" && (
+            <h3>
+              Are The ingredients Gluten-free? {isGlutenFree}
+            </h3>
+          )}
+        </Box >
+        <Box   sx={{
+                position: 'absolute',
+                bottom: 0,
+                left: '50%',
+                transform: 'translateX(-50%)'
+              }}>
           <NavBar />
         </Box>
       </Stack>
     </ThemeProvider>
-  );
+    );
 };
 
 export default Home;
