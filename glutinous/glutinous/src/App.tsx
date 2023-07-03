03277d265803c5b363b7734d62d0a573f4d0bd47
Mitchell Wagner <pim128@usask.ca>
2023-07-02 21:43:04 -0600

    Chat API

--- glutinous/glutinous/src/App.tsx
+++ glutinous/glutinous/src/App.tsx
@@ -1,16 +1,71 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { Button, Stack } from '@mui/material';
 import Home from './pages/Home';
-import { Stack } from '@mui/material';
-import SearchButton from './components/Buttons/SearchButton';
-const App = () => {
 
+const API_KEY = "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"; // Replace with your actual API key
+
+const App = () => {
+  const [ingredients, setIngredients] = useState("");
+  const [isGlutenFree, setIsGlutenFree] = useState("");
+
+  async function callOpenAIAPI() {
+    console.log("Calling API");
+
+    const prompt = `Is ${ingredients} gluten-free, Yes or No?`;
+
+    const APIBody = {
+      model: "text-davinci-003",
+      prompt,
+      temperature: 0,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ["\n"]
+    };
+
+    try {
+      const response = await fetch("https://api.openai.com/v1/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify(APIBody)
+      });
+
+      const data = await response.json();
+      console.log(data);
+
+      if (data.choices && data.choices.length > 0) {
+        const generatedText = data.choices[0].text.trim();
+        setIsGlutenFree(generatedText);
+      } else {
+        setIsGlutenFree("No response from the API");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setIsGlutenFree("API call failed");
+    }
+  }
 
   return (
-    <Stack>
+    <Stack className="bgImage">
       <Home />
+      <textarea
+        onChange={(e) => setIngredients(e.target.value)}
+        placeholder='Enter Your Ingredients List'
+        cols={75}
+        rows={10}
+      />
+      <Button onClick={callOpenAIAPI}>
+        SCAN
+      </Button>
+      {isGlutenFree !== "" && (
+        <h3>This food {isGlutenFree} gluten-free</h3>
+      )}
     </Stack>
-  )
+  );
 };
 
 export default App;
