f31b53711135a7319b81a0ac1c5df2a0c98c3b87
Mitchell Wagner <pim128@usask.ca>
2023-07-03 21:16:25 -0600

    Fixed stuff

--- glutinous/glutinous/src/pages/Home.tsx
+++ glutinous/glutinous/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 import { Stack, Box, ThemeProvider } from '@mui/material';
 import ScanPage from './Scan';
 import SearchPage from './SearchPage';
