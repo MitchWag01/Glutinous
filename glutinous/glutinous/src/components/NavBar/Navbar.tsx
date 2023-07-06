d63dae8039b35bb1c30715b5dbc6b5952a702b96
Mitchell Wagner <pim128@usask.ca>
2023-07-05 23:04:05 -0600

    Fixed a lot.....

--- glutinous/glutinous/src/components/NavBar/Navbar.tsx
+++ glutinous/glutinous/src/components/NavBar/Navbar.tsx
@@ -1,57 +1,119 @@
 import * as React from 'react';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
-import { HomeRounded, LiveHelpRounded } from '@mui/icons-material';
-import { ThemeProvider, styled } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import theme from '../../themes/theme';
+import { HomeRounded, LiveHelpRounded, Search, History } from '@mui/icons-material';
 
-const StyledBottomNavigation = styled(BottomNavigation)(({ theme }) => ({
-  width: 500,
-  backgroundColor: theme.palette.primary.main, // Use backgroundColor instead of bgcolor
-  borderRadius: 10,
-  '& .MuiBottomNavigationAction-root': {
-    color: theme.palette.secondary.main,
-    borderRadius: 5,
-    '&.Mui-selected': {
-      backgroundColor: 'transparent',
-    },
-    '&:focus': {
-      outline: '2px solid transparent',
-    },
-  },
-  '& .MuiSvgIcon-root': {
-    fontSize: '35px',
-  },
-}));
+enum NavButton {
+  HOME = 'home',
+  SEARCH = 'search',
+  RECENT_SEARCHES = 'recent_searches',
+  HELP = 'help',
+}
 
-export default function NavBar() {
-  const [value, setValue] = React.useState('recents');
+interface NavBarProps {
+  onNavButtonClick: (button: NavButton) => void;
+  selectedButton: NavButton;
+}
 
+const NavBar: React.FC<NavBarProps> = ({ onNavButtonClick, selectedButton }) => {
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-    console.log(event);
+    onNavButtonClick(newValue as NavButton);
+  };
+
+  const renderIcon = (button: NavButton) => {
+    const iconSize = 30; // Adjust the size as desired
+    if (button === NavButton.HOME && selectedButton === NavButton.HOME) {
+      return <img src="../../../public/images/LogoMakr-2ND0aW.png" alt="Home" style={{ width: `${iconSize}px`, height: `${iconSize}px` }} />;
+    } else if (button === NavButton.SEARCH && selectedButton === NavButton.SEARCH) {
+      return <img src="../../../public/images/LogoMakr-2ND0aW.png" alt="Search" style={{ width: `${iconSize}px`, height: `${iconSize}px` }} />;
+    } else if (button === NavButton.RECENT_SEARCHES && selectedButton === NavButton.RECENT_SEARCHES) {
+      return <img src="../../../public/images/LogoMakr-2ND0aW.png" alt="Recent" style={{ width: `${iconSize}px`, height: `${iconSize}px` }} />;
+    } else if (button === NavButton.HELP && selectedButton === NavButton.HELP) {
+      return <img src="../../../public/images/LogoMakr-2ND0aW.png" alt="Help" style={{ width: `${iconSize}px`, height: `${iconSize}px` }} />;
+    }
+
+    switch (button) {
+      case NavButton.HOME:
+        return <HomeRounded sx={{ fontSize: `${iconSize}px`, color: 'secondary.main' }} />;
+        case NavButton.SEARCH:
+          return <Search sx={{ fontSize: `${iconSize}px`, color: 'secondary.main' }} />;
+          case NavButton.RECENT_SEARCHES:
+            return <History sx={{ fontSize: `${iconSize}px`, color: 'secondary.main' }} />;
+
+      case NavButton.HELP:
+        return <LiveHelpRounded sx={{ fontSize: `${iconSize}px`, color: 'secondary.main' }} />;
+        default:
+        return null;
+    }
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <StyledBottomNavigation value={value} onChange={handleChange}>
+      <BottomNavigation
+        sx={{
+          width: 400,
+          bgcolor: 'primary.main',
+          borderRadius: 10,
+          '& .MuiBottomNavigationAction-root': {
+            color: 'secondary.main',
+            borderRadius: 5,
+            '&.Mui-selected': {
+              backgroundColor: 'transparent',
+            },
+            '&:focus': {
+              outline: '2px solid transparent', // Set a transparent outline
+            },
+          },
+          pl:2,pr:2
+        }}
+        value={selectedButton}
+        onChange={handleChange}
+      >
         <BottomNavigationAction
           label="Home"
-          value="home"
-          icon={<HomeRounded sx={{ color: theme.palette.secondary.main }} />}
+          value={NavButton.HOME}
+          icon={renderIcon(NavButton.HOME)}
+          sx={{
+            '& .MuiBottomNavigationAction-label': {
+              color: selectedButton === NavButton.HOME ? theme.palette.secondary.main : 'inherit',
+            },
+          }}
         />
         <BottomNavigationAction
-          label="Recent Searches"
-          value="recent_searches"
-          icon={<RestoreIcon sx={{ color: theme.palette.secondary.main }} />}
+          label="Search"
+          value={NavButton.SEARCH}
+          icon={renderIcon(NavButton.SEARCH)}
+          sx={{
+            '& .MuiBottomNavigationAction-label': {
+              color: selectedButton === NavButton.SEARCH ? theme.palette.secondary.main : 'inherit',
+            },
+          }}
+        />
+        <BottomNavigationAction
+          label="Recent"
+          value={NavButton.RECENT_SEARCHES}
+          icon={renderIcon(NavButton.RECENT_SEARCHES)}
+          sx={{
+            '& .MuiBottomNavigationAction-label': {
+              color: selectedButton === NavButton.RECENT_SEARCHES ? theme.palette.secondary.main : 'inherit',
+            },
+          }}
         />
         <BottomNavigationAction
           label="Help"
-          value="help"
-          icon={<LiveHelpRounded sx={{ color: theme.palette.secondary.main }} />}
+          value={NavButton.HELP}
+          icon={renderIcon(NavButton.HELP)}
+          sx={{
+            '& .MuiBottomNavigationAction-label': {
+              color: selectedButton === NavButton.HELP ? theme.palette.secondary.main : 'inherit',
+            },
+          }}
         />
-      </StyledBottomNavigation>
+      </BottomNavigation>
     </ThemeProvider>
   );
-}
+};
+
+export default NavBar;
--- glutinous/glutinous/src/pages/Home.tsx
+++ glutinous/glutinous/src/pages/Home.tsx
@@ -1,57 +1,19 @@
-import { useState} from 'react';
 import { Stack, Box, ThemeProvider } from '@mui/material';
-import ScanPage from './Scan';
-import SearchPage from './SearchPage';
 import theme from '../themes/theme';
-import NavBar from '../components/NavBar/Navbar';
 import CameraButton from '../components/Buttons/CameraButton';
-import Header from '../components/Headers/MainHeader';
-
 
 const Home = () => {
-
-
-  const [ScanState, setScanState] = useState(false);
-  const [SearchState, setSearchState] = useState(false);
-  const [searchList, setSearchList] = useState<string[]>([]);
-
-  // setting the state for user warning and acknowledgement
-
-
-
-
-  console.log(setScanState)
-  console.log(setSearchList)
-  console.log(setSearchState)
-
-
-    //onClick={callOpenAIAPI}
-  
-
   return (
     <ThemeProvider theme={theme}>
-      <Stack>
-        <Header></Header>
-  
-        {ScanState && <ScanPage ingredientsList="Jesse's bread preferences are a terror to our society as he likes raisin bread..." />}
-        {SearchState && <SearchPage searchlist={searchList} />} 
-        
+      <Stack>        
         <Box       sx={{
         position: 'absolute',
-        top: '50%',
-        left: '60%',
+        top: '60%',
+        left: '64%',
         transform: 'translate(-50%, -50%)'
       }}>
-        <CameraButton></CameraButton>
-        </Box >
-        <Box   sx={{
-                position: 'absolute',
-                bottom: 0,
-                left: '50%',
-                transform: 'translateX(-50%)'
-              }}>
-          <NavBar />
-        </Box>
+             <CameraButton></CameraButton>
+        </Box > 
       </Stack>
     </ThemeProvider>
     );
