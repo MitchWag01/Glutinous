53cbdf66ebeb9c6974117e20bd395caacc18779b
Mitchell Wagner <pim128@usask.ca>
2023-07-09 00:08:26 -0600

    Added User Settings

---
+++ glutinous/glutinous/src/components/UserMenu/menuItems.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
+import HistoryIcon from '@mui/icons-material/History';
+import PersonIcon from '@mui/icons-material/Person';
+import ContactMailIcon from '@mui/icons-material/ContactMail';
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
+import { Link } from 'react-router-dom';
+import { Typography } from '@mui/material';
+
+export const mainListItems = (
+  <React.Fragment>
+    <ListItemButton component={Link} to="/">
+      <ListItemIcon>
+        <DocumentScannerIcon />
+      </ListItemIcon>
+      <ListItemText primary="Home" />
+    </ListItemButton>
+    <ListItemButton component={Link} to="/menu">
+      <ListItemIcon>
+        <HistoryIcon />
+      </ListItemIcon>
+      <ListItemText primary="Purchase History" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <PersonIcon />
+      </ListItemIcon>
+      <ListItemText primary="User Information" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <ContactMailIcon />
+      </ListItemIcon>
+      <ListItemText primary="Contact Us" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <PriorityHighIcon />
+      </ListItemIcon>
+      <ListItemText primary="Disclaimer" />
+    </ListItemButton>
+  </React.Fragment>
+);
+
+export const secondaryListItems = (
+  <React.Fragment>
+    <ListSubheader component="div" inset>
+    </ListSubheader>
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Recent Updates" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Data Usage" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Donate" />
+    </ListItemButton>
+  </React.Fragment>
+);
